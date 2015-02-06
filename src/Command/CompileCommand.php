a7c9aece13cd169f3f7100496b4f40fcb437d6ce
Nate Brunette <n@tebru.net>
2015-02-05 20:13:46 -0600

    Broke class compilation into service resolver
    Class is responsible for finding all services in a given directory recursively.
    
    Signed-off-by: Nate Brunette <n@tebru.net>

--- src/Command/CompileCommand.php
+++ src/Command/CompileCommand.php
@@ -5,14 +5,11 @@
 
 namespace Tebru\Retrofit\Command;
 
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
-use RecursiveRegexIterator;
-use RegexIterator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Tebru\Retrofit\Finder\ServiceResolver;
 use Tebru\Retrofit\Retrofit;
 
 /**
@@ -35,46 +32,14 @@ class CompileCommand extends Command
         $srcDir = $input->getArgument('sourceDirectory');
         $cacheDir = $input->getArgument('cacheDirectory');
 
+        $serviceResolver = new ServiceResolver();
+        $services = $serviceResolver->findServices($srcDir);
+
         $retrofit = new Retrofit($cacheDir);
-
-        $directory = new RecursiveDirectoryIterator($srcDir);
-        $iterator = new RecursiveIteratorIterator($directory);
-        $files = new RegexIterator($iterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
-
-        $matches = 0;
-        foreach ($files as $file) {
-            $regex = '/Tebru\\\\Retrofit\\\\Annotation/';
-            $fileString = file_get_contents($file[0]);
-            $matchesFound = preg_match($regex, $fileString);
-
-            if (!$matchesFound) {
-                continue;
-            }
-
-            $namespaceRegex = '/^namespace\s+([\w\\\\]+)/m';
-            $interfaceRegex = '/^interface\s+([\w\\\\]+)[\s{\n]?/m';
-            $namespaceMatchesFound = preg_match($namespaceRegex, $fileString, $namespaceMatches);
-            $interfaceMatchesFound = preg_match($interfaceRegex, $fileString, $interfaceMatches);
-
-            if (!$interfaceMatchesFound) {
-                continue;
-            }
-
-            $matches++;
-
-            $className = '';
-
-            if ($namespaceMatchesFound) {
-                $className .= '\\' . $namespaceMatches[1];
-            }
-
-            $className .= '\\' . $interfaceMatches[1];
-
-            $retrofit->registerService($className);
-        }
-
+        $retrofit->registerServices($services);
         $retrofit->createCache();
 
+        $matches = count($services);
         $output->writeln(sprintf('<info>Compiled %s %s successfully</info>', $matches, ($matches === 1) ? 'class' : 'classes'));
     }
 }
---
+++ src/Finder/ServiceResolver.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * File ServiceResolver.php 
+ */
+
+namespace Tebru\Retrofit\Finder;
+
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RecursiveRegexIterator;
+use RegexIterator;
+
+/**
+ * Class ServiceResolver
+ *
+ * @author Nate Brunette <n@tebru.net>
+ */
+class ServiceResolver
+{
+    /**
+     * Find all services given a source directory
+     *
+     * @param string $srcDir
+     * @return array
+     */
+    public function findServices($srcDir)
+    {
+        $directory = new RecursiveDirectoryIterator($srcDir);
+        $iterator = new RecursiveIteratorIterator($directory);
+        $files = new RegexIterator($iterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
+
+        $services = [];
+        foreach ($files as $file) {
+            $regex = '/Tebru\\\\Retrofit\\\\Annotation/';
+            $fileString = file_get_contents($file[0]);
+            $matchesFound = preg_match($regex, $fileString);
+
+            if (!$matchesFound) {
+                continue;
+            }
+
+            $namespaceRegex = '/^namespace\s+([\w\\\\]+)/m';
+            $interfaceRegex = '/^interface\s+([\w\\\\]+)[\s{\n]?/m';
+            $namespaceMatchesFound = preg_match($namespaceRegex, $fileString, $namespaceMatches);
+            $interfaceMatchesFound = preg_match($interfaceRegex, $fileString, $interfaceMatches);
+
+            if (!$interfaceMatchesFound) {
+                continue;
+            }
+
+            $className = '';
+
+            if ($namespaceMatchesFound) {
+                $className .= '\\' . $namespaceMatches[1];
+            }
+
+            $className .= '\\' . $interfaceMatches[1];
+
+            $services[] = $className;
+        }
+
+        return $services;
+    }
+}
