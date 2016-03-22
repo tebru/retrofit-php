e61df918a9d981911c30f76000ff11dd3647540a
Nate Brunette <n@tebru.net>
2016-03-22 15:17:17 -0500

    Updating dynamo library and adding missing test in php 7
    Signed-off-by: Nate Brunette <n@tebru.net>

--- tests/Unit/Generation/Builder/MethodBodyBuilderTest.php
+++ tests/Unit/Generation/Builder/MethodBodyBuilderTest.php
@@ -178,6 +178,24 @@ class MethodBodyBuilderTest extends MockeryTestCase
         $this->assertSame($expected, $response);
     }
 
+    public function testCanBuildPostRequestPartsJsonEncoded()
+    {
+        $builder = new MethodBodyBuilder();
+        $builder->setRequestMethod('POST');
+        $builder->setBaseUrl('http://example.com');
+        $builder->setUri('/path');
+        $builder->setBodyParts(['foo' => 'bar']);
+        $builder->setJsonEncode(true);
+        $context['serializeNull'] = true;
+        $builder->setDeserializationContext($context);
+        $builder->setReturnType(MockUser::class);
+
+        $response = $builder->build();
+        $expected = '$requestUrl = http://example.com . "/path";$headers = [];$body = json_encode(["foo" => "bar"]);$request = new \GuzzleHttp\Psr7\Request("POST", $requestUrl, $headers, $body);$this->logger->debug("Created Request", ["request" => ["method" => $request->getMethod(), "uri" => rawurldecode((string)$request->getUri()), "headers" => $request->getHeaders(), "body" => (string)$request->getBody()]]);$this->logger->info("Dispatching BeforeSendEvent");$beforeSendEvent = new \Tebru\Retrofit\Event\BeforeSendEvent($request);$this->eventDispatcher->dispatch("retrofit.beforeSend", $beforeSendEvent);$request = $beforeSendEvent->getRequest();try {$this->logger->info("Sending Synchronous Request");$response = $this->client->send($request);} catch (\Exception $exception) {$this->logger->error("Caught Exception", ["exception" => $exception]);$this->logger->info("Dispatching ApiExceptionEvent");$apiExceptionEvent = new \Tebru\Retrofit\Event\ApiExceptionEvent($exception, $request);$this->eventDispatcher->dispatch("retrofit.apiException", $apiExceptionEvent);$exception = $apiExceptionEvent->getException();throw new \Tebru\Retrofit\Exception\RetrofitApiException(get_class($this), $exception->getMessage(), $exception->getCode(), $exception);}$this->logger->debug("API Response", ["response" => $response]);$this->logger->info("Dispatching AfterSendEvent");$afterSendEvent = new \Tebru\Retrofit\Event\AfterSendEvent($request, $response);$this->eventDispatcher->dispatch("retrofit.afterSend", $afterSendEvent);$response = $afterSendEvent->getResponse();$context = \JMS\Serializer\DeserializationContext::create();$context->setSerializeNull(1);$return = $this->serializer->deserialize((string)$response->getBody(), "Tebru\Retrofit\Test\Mock\MockUser", "json", $context);$this->logger->info("Dispatching ReturnEvent");$returnEvent = new \Tebru\Retrofit\Event\ReturnEvent($return);$this->eventDispatcher->dispatch("retrofit.return", $returnEvent);return $returnEvent->getReturn();';
+
+        $this->assertSame($expected, $response);
+    }
+
     public function testBodyObjectFormEncoded()
     {
         $builder = new MethodBodyBuilder();
